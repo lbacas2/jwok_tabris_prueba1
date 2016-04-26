cfd443fc050fea90618f9a429cb32506d184c52c
lbacas2 <luis.bacas.riveiro@netzima.com>
2016-04-26 11:58:14 +0200

    Add hello world demo.

---
+++ hello.js
@@ -0,0 +1,20 @@
+var page = new tabris.Page({
+  title: "Hello, World!",
+  topLevel: true
+});
+
+var button = new tabris.Button({
+  text: "Native Widgets",
+  layoutData: {centerX: 0, top: 100}
+}).appendTo(page);
+
+var label = new tabris.TextView({
+  font: "24px",
+  layoutData: {centerX: 0, top: [button, 50]}
+}).appendTo(page);
+
+button.on("select", function() {
+  label.set("text", "Totally Rock!");
+});
+
+page.open();
